f77dc872b1cf4aa415ce36e3874f91a97da6fbe7
Ermek Abilov <ermek.abilove@gmail.com>
2024-05-27 13:38:37 +0600

    add backend architecture

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Book : Entity
+{
+    public int Title { get; set; }
+
+    public string Genre { get; set; }
+    public string AuthorName { get; set; }
+    public int Year { get; set; }
+}
